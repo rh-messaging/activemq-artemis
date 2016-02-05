44eab74d45392148d05faf33e972323865b4ece7
Clebert Suconic <clebertsuconic@apache.org>
2016-02-05 16:42:34 -0500

    ARTEMIS-392 Fixing Allocator after socket upgrade
    https://issues.apache.org/jira/browse/ARTEMIS-392
    (cherry picked from commit e9ce4ce40beed4bf974f8542ee86222c4e06ade5)
    
    https://issues.jboss.org/browse/JBEAP-3285

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/protocol/ProtocolHandler.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/protocol/ProtocolHandler.java
@@ -44,6 +44,7 @@ import org.apache.activemq.artemis.core.remoting.impl.netty.HttpKeepAliveRunnabl
 import org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptor;
 import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnector;
 import org.apache.activemq.artemis.core.remoting.impl.netty.NettyServerConnection;
+import org.apache.activemq.artemis.core.remoting.impl.netty.PartialPooledByteBufAllocator;
 import org.apache.activemq.artemis.core.remoting.impl.netty.TransportConstants;
 import org.apache.activemq.artemis.spi.core.protocol.ProtocolManager;
 import org.apache.activemq.artemis.utils.ConfigurationHelper;
@@ -172,6 +173,11 @@ public class ProtocolHandler {
          NettyServerConnection connection = channelHandler.createConnection(ctx, protocolToUse, httpEnabled);
          protocolManagerToUse.handshake(connection, new ChannelBufferWrapper(in));
          pipeline.remove(this);
+
+         // https://issues.apache.org/jira/browse/ARTEMIS-392
+         // Application servers or other components may upgrade a regular socket to Netty
+         // We need to be able to work normally as with anything else on Artemis
+         ctx.channel().config().setAllocator(PartialPooledByteBufAllocator.INSTANCE);
          ctx.flush();
       }
 
