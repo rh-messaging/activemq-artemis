109ef60275648be13c9e82bb9411aa073e5216bb
Clebert Suconic <clebertsuconic@apache.org>
2016-04-20 09:51:44 -0400

    ARTEMIS-490 Fixing new largeMessage copy
    (cherry picked from commit b89faae45811454394233bf683d2ce17d373034a)
    
    https://issues.jboss.org/browse/JBEAP-4225

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/persistence/impl/journal/LargeServerMessageImpl.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/persistence/impl/journal/LargeServerMessageImpl.java
@@ -270,6 +270,10 @@ public final class LargeServerMessageImpl extends ServerMessageImpl implements L
       try {
          LargeServerMessage newMessage = storageManager.createLargeMessage(newID, this);
 
+         boolean originallyOpen = file != null && file.isOpen();
+
+         validateFile();
+
 
          byte[] bufferBytes = new byte[100 * 1024];
 
@@ -277,7 +281,6 @@ public final class LargeServerMessageImpl extends ServerMessageImpl implements L
 
          long oldPosition = file.position();
 
-         boolean originallyOpen = file.isOpen();
          file.open();
          file.position(0);
 
@@ -288,7 +291,7 @@ public final class LargeServerMessageImpl extends ServerMessageImpl implements L
             int bytesRead = file.read(buffer);
 
             byte[] bufferToWrite;
-            if (bytesRead == 0) {
+            if (bytesRead <= 0) {
                break;
             }
             else if (bytesRead == bufferBytes.length) {
