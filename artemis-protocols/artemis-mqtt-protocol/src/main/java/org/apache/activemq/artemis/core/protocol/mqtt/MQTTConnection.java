8a90044206e2fde79419fbf6ec250808e516b968
Clebert Suconic <clebertsuconic@apache.org>
2016-09-02 16:30:44 -0400

    ARTEMIS-581 Implement max disk usage, and global-max-size
    max-disk-usage = how much of a disk we can use before the system blocks
    global-max-size = how much bytes we can take from memory for messages before we start enter into the configured page mode
    
    This will also change the default created configuration into page-mode as that's more reliable for systems.
    
    https://issues.jboss.org/browse/ENTMQ-1795 and https://issues.jboss.org/browse/ENTMQ-1794

--- artemis-protocols/artemis-mqtt-protocol/src/main/java/org/apache/activemq/artemis/core/protocol/mqtt/MQTTConnection.java
+++ artemis-protocols/artemis-mqtt-protocol/src/main/java/org/apache/activemq/artemis/core/protocol/mqtt/MQTTConnection.java
@@ -215,4 +215,9 @@ public class MQTTConnection implements RemotingConnection {
    public boolean isSupportReconnect() {
       return false;
    }
+
+   @Override
+   public boolean isSupportsFlowControl() {
+      return false;
+   }
 }
