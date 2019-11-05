4d50d9dcb18776cc71c282dad0c31cc1a70fe076
brusdev <bruscinodf@gmail.com>
2019-11-05 13:45:39 +0100

    ARTEMIS-2538 Removing all messages from a huge queue causes OOM
    The iterQueue transaction commits are locked by the synchronization
    context. So removing all messages from a huge queue causes the creation
    of too locked transactions for the paged messages and so OOM.
    The iteration on paged message is executed out the iterQueue
    synchronization context to avoid to lock the transaction commits.
    
    (cherry picked from commit 55a55daa7b84b390d79dc0a7747b176ad322ec67)
    
    Conflicts:
    artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/QueueImpl.java
    
    downstream: ENTMQBR-3112

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/QueueImpl.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/QueueImpl.java
@@ -221,6 +221,8 @@ public class QueueImpl extends CriticalComponentImpl implements Queue {
    //This lock is used to prevent deadlocks between direct and async deliveries
    private final ReentrantLock deliverLock = new ReentrantLock();
 
+   private final ReentrantLock depageLock = new ReentrantLock();
+
    private volatile boolean depagePending = false;
 
    private final StorageManager storageManager;
@@ -1863,48 +1865,53 @@ public class QueueImpl extends CriticalComponentImpl implements Queue {
       int count = 0;
       int txCount = 0;
 
-      Transaction tx = new TransactionImpl(storageManager);
+      depageLock.lock();
 
-      synchronized (this) {
-         try (LinkedListIterator<MessageReference> iter = iterator()) {
+      try {
+         Transaction tx = new TransactionImpl(storageManager);
 
-            while (iter.hasNext()) {
-               MessageReference ref = iter.next();
+         synchronized (this) {
 
-               if (ref.isPaged() && queueDestroyed) {
-                  // this means the queue is being removed
-                  // hence paged references are just going away through
-                  // page cleanup
-                  continue;
+            try (LinkedListIterator<MessageReference> iter = iterator()) {
+
+               while (iter.hasNext()) {
+                  MessageReference ref = iter.next();
+
+                  if (ref.isPaged() && queueDestroyed) {
+                     // this means the queue is being removed
+                     // hence paged references are just going away through
+                     // page cleanup
+                     continue;
+                  }
+
+                  if (filter1 == null || filter1.match(ref.getMessage())) {
+                     messageAction.actMessage(tx, ref);
+                     iter.remove();
+                     txCount++;
+                     count++;
+                  }
                }
 
-               if (filter1 == null || filter1.match(ref.getMessage())) {
-                  messageAction.actMessage(tx, ref);
-                  iter.remove();
-                  txCount++;
+               if (txCount > 0) {
+                  tx.commit();
+
+                  tx = new TransactionImpl(storageManager);
+
+                  txCount = 0;
+               }
+
+               List<MessageReference> cancelled = scheduledDeliveryHandler.cancel(filter1);
+               for (MessageReference messageReference : cancelled) {
+                  messageAction.actMessage(tx, messageReference, false);
                   count++;
+                  txCount++;
                }
-            }
 
-            if (txCount > 0) {
-               tx.commit();
-
-               tx = new TransactionImpl(storageManager);
-
-               txCount = 0;
-            }
-
-            List<MessageReference> cancelled = scheduledDeliveryHandler.cancel(filter1);
-            for (MessageReference messageReference : cancelled) {
-               messageAction.actMessage(tx, messageReference, false);
-               count++;
-               txCount++;
-            }
-
-            if (txCount > 0) {
-               tx.commit();
-               tx = new TransactionImpl(storageManager);
-               txCount = 0;
+               if (txCount > 0) {
+                  tx.commit();
+                  tx = new TransactionImpl(storageManager);
+                  txCount = 0;
+               }
             }
          }
 
@@ -1939,6 +1946,8 @@ public class QueueImpl extends CriticalComponentImpl implements Queue {
          }
 
          return count;
+      } finally {
+         depageLock.unlock();
       }
    }
 
@@ -3723,10 +3732,13 @@ public class QueueImpl extends CriticalComponentImpl implements Queue {
 
       @Override
       public void run() {
+         depageLock.lock();
          try {
             depage(scheduleExpiry);
          } catch (Exception e) {
             ActiveMQServerLogger.LOGGER.errorDelivering(e);
+         } finally {
+            depageLock.unlock();
          }
       }
    }
