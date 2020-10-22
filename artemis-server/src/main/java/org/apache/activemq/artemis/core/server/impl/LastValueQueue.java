f93e2a8c98b94e3054afe127aa8a2df71886b932
Clebert Suconic <clebertsuconic@apache.org>
2020-10-22 13:32:19 -0400

    ARTEMIS-2927 LVQ broken after restart
    (cherry picked from commit f6ef285859347a57749ac4e5881d879df23e4f88)
    
    Conflicts:
    artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/LastValueQueue.java
    
    downstream: ENTMQBR-4420

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/LastValueQueue.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/LastValueQueue.java
@@ -220,6 +220,19 @@ public class LastValueQueue extends QueueImpl {
       super.acknowledge(ref, reason, consumer);
    }
 
+   @Override
+   public synchronized void reload(final MessageReference ref) {
+      // repopulate LVQ map & reload proper HolderReferences
+      SimpleString lastValueProp = ref.getLastValueProperty();
+      if (lastValueProp != null) {
+         HolderReference hr = new HolderReference(lastValueProp, ref);
+         map.put(lastValueProp, hr);
+         super.reload(hr);
+      } else {
+         super.reload(ref);
+      }
+   }
+
    private synchronized void removeIfCurrent(MessageReference ref) {
       SimpleString lastValueProp = ref.getLastValueProperty();
       if (lastValueProp != null) {
@@ -462,6 +475,11 @@ public class LastValueQueue extends QueueImpl {
       @Override
       public long getPersistentSize() throws ActiveMQException {
          return ref.getPersistentSize();
+      }
+
+      @Override
+      public String toString() {
+         return new StringBuilder().append("HolderReference").append("@").append(Integer.toHexString(System.identityHashCode(this))).append("[ref=").append(ref).append("]").toString();
       }
    }
 
