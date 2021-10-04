fc4f8e8158e3552adcfc737c2c085707a2c2f394
Clebert Suconic <clebertsuconic@apache.org>
2021-10-04 18:50:42 -0400

    ARTEMIS-3513 Compacting exception invalidates deletes and updates
    (cherry picked from commit ef63dc95bbf264cf108f1b324aa6bcb264cb267b)
    
    downstream: ENTMQBR-5565

--- artemis-journal/src/main/java/org/apache/activemq/artemis/core/journal/Journal.java
+++ artemis-journal/src/main/java/org/apache/activemq/artemis/core/journal/Journal.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.activemq.artemis.core.io.IOCriticalErrorListener;
 import org.apache.activemq.artemis.core.io.SequentialFileFactory;
 import org.apache.activemq.artemis.core.journal.impl.JournalFile;
 import org.apache.activemq.artemis.core.persistence.Persister;
@@ -123,6 +124,14 @@ public interface Journal extends ActiveMQComponent {
    void appendUpdateRecord(long id, byte recordType, Persister persister, Object record, boolean sync) throws Exception;
 
    void tryAppendUpdateRecord(long id, byte recordType, Persister persister, Object record, JournalUpdateCallback updateCallback, boolean sync, boolean replaceableUpdate) throws Exception;
+
+   default IOCriticalErrorListener getCriticalErrorListener() {
+      return null;
+   }
+
+   default Journal setCriticalErrorListener(IOCriticalErrorListener criticalErrorListener) {
+      return this;
+   }
 
    default void appendUpdateRecord(long id,
                                    byte recordType,
