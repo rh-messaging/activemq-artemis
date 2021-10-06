fef6b41031c78ace6e567a47e50664548fb2e9c3
Domenico Francesco Bruscino <brusdev@apache.org>
2021-10-06 06:34:58 +0200

    ARTEMIS-3502 Fix ActiveMQServerImpl indentation
    Fix ActiveMQServerImpl indentation from ac91080d2887081aa38386e0f82bc3e1e3807888

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/ActiveMQServerImpl.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/server/impl/ActiveMQServerImpl.java
@@ -3770,13 +3770,13 @@ public class ActiveMQServerImpl implements ActiveMQServer {
 
          final Queue queue = queueFactory.createQueueWith(queueConfiguration, pagingManager);
 
-      if (queueConfiguration.isTransient()) {
-         queue.setConsumersRefCount(new TransientQueueManagerImpl(this, queue.getName()));
-      } else {
-         queue.setConsumersRefCount(new QueueManagerImpl(this, queue.getName()));
-      }
+         if (queueConfiguration.isTransient()) {
+            queue.setConsumersRefCount(new TransientQueueManagerImpl(this, queue.getName()));
+         } else {
+            queue.setConsumersRefCount(new QueueManagerImpl(this, queue.getName()));
+         }
 
-      final QueueBinding localQueueBinding = new LocalQueueBinding(queue.getAddress(), queue, nodeManager.getNodeId());
+         final QueueBinding localQueueBinding = new LocalQueueBinding(queue.getAddress(), queue, nodeManager.getNodeId());
 
          long txID = 0;
          if (queue.isDurable()) {
