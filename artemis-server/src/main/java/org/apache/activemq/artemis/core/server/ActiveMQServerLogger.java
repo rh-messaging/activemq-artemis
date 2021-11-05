2f72fba9dc238505d041320171a3bcbe55e24b27
Clebert Suconic <clebertsuconic@apache.org>
2021-11-04 20:56:06 -0400

    ARTEMIS-3554 Invalid Prepared Transaction could interrupt server reload
    (cherry picked from commit 98a6e42a57038e2a97d13c2cf1d3a70dd30f5ae1)
    
    downstream: ENTMQBR-5725 and ENTMQBR-5728
    (cherry picked from commit b1bff518d080538a74670ba8302ae2dcb6678132)

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/server/ActiveMQServerLogger.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/server/ActiveMQServerLogger.java
@@ -1744,6 +1744,11 @@ public interface ActiveMQServerLogger extends BasicLogger {
    @Message(id = 222303, value = "Redistribution by {0} of messageID = {1} failed", format = Message.Format.MESSAGE_FORMAT)
    void errorRedistributing(@Cause Throwable t, String queueName, long m);
 
+   @LogMessage(level = Logger.Level.WARN)
+   @Message(id = 222306, value = "Failed to load prepared TX and it will be rolled back: {0}",
+      format = Message.Format.MESSAGE_FORMAT)
+   void failedToLoadPreparedTX(@Cause Throwable e, String message);
+
    @LogMessage(level = Logger.Level.ERROR)
    @Message(id = 224000, value = "Failure in initialisation", format = Message.Format.MESSAGE_FORMAT)
    void initializationError(@Cause Throwable e);
