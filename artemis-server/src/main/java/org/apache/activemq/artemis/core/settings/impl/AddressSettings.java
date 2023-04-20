rh-messaging/activemq-artemis

commit d61126bb380c3de72422a3b60d2ed6033e1ca107

ARTEMIS-4162 support deleting addresses & queues w/o usage check
There are certain use-cases where addresses will be auto-created and
never have a direct binding created on them. Because of this they will
never be auto-deleted. If a large number of these addresses build up
they will consume a problematic amount of heap space.

One specific example of this use-case is an MQTT subscriber with a
wild-card subscription and a large number of MQTT producers sending one
or two messages a large number of different MQTT topics covered by the
wild-card. Since no bindings are ever created on any of these individual
addresses (e.g. from a subscription queue) they will never be
auto-deleted, but they will eventually consume a large amount of heap.
The only way to deal with these addresses is to manually delete them.

There are also situations  where queues may be created and never have
any messages sent to them or never have a consumer connect. These
queues will never be auto-deleted so they must be deleted manually.

This commit adds the ability to configure the broker to skip the usage
check so that these kinds of addresses and queues can be deleted
automatically.

(cherry picked from commit b76c67230538ff3b366793f7194bc4539b7620e7)

downstream: ENTMQBR-7866

artemis-server/src/main/java/org/apache/activemq/artemis/core/settings/impl/AddressSettings.java
@@ -80,6 +80,8 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
 
    public static final long DEFAULT_AUTO_DELETE_QUEUES_DELAY = 0;
 
+   public static final boolean DEFAULT_AUTO_DELETE_QUEUES_SKIP_USAGE_CHECK = false;
+
    public static final long DEFAULT_AUTO_DELETE_QUEUES_MESSAGE_COUNT = 0;
 
    public static final DeletionPolicy DEFAULT_CONFIG_DELETE_QUEUES = DeletionPolicy.OFF;
@@ -89,6 +91,8 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
    public static final boolean DEFAULT_AUTO_DELETE_ADDRESSES = true;
 
    public static final long DEFAULT_AUTO_DELETE_ADDRESSES_DELAY = 0;
+
+   public static final boolean DEFAULT_AUTO_DELETE_ADDRESSES_SKIP_USAGE_CHECK = false;
 
    public static final DeletionPolicy DEFAULT_CONFIG_DELETE_ADDRESSES = DeletionPolicy.OFF;
 
@@ -219,6 +223,8 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
 
    private Long autoDeleteQueuesDelay = null;
 
+   private Boolean autoDeleteQueuesSkipUsageCheck = null;
+
    private Long autoDeleteQueuesMessageCount = null;
 
    private Long defaultRingSize = null;
@@ -232,6 +238,8 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
    private Boolean autoDeleteAddresses = null;
 
    private Long autoDeleteAddressesDelay = null;
+
+   private Boolean autoDeleteAddressesSkipUsageCheck = null;
 
    private DeletionPolicy configDeleteAddresses = null;
 
@@ -318,10 +326,12 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
       this.autoDeleteQueues = other.autoDeleteQueues;
       this.autoDeleteCreatedQueues = other.autoDeleteCreatedQueues;
       this.autoDeleteQueuesDelay = other.autoDeleteQueuesDelay;
+      this.autoDeleteQueuesSkipUsageCheck = other.autoDeleteQueuesSkipUsageCheck;
       this.configDeleteQueues = other.configDeleteQueues;
       this.autoCreateAddresses = other.autoCreateAddresses;
       this.autoDeleteAddresses = other.autoDeleteAddresses;
       this.autoDeleteAddressesDelay = other.autoDeleteAddressesDelay;
+      this.autoDeleteAddressesSkipUsageCheck = other.autoDeleteAddressesSkipUsageCheck;
       this.configDeleteAddresses = other.configDeleteAddresses;
       this.configDeleteDiverts = other.configDeleteDiverts;
       this.managementBrowsePageSize = other.managementBrowsePageSize;
@@ -429,6 +439,15 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
       return this;
    }
 
+   public boolean getAutoDeleteQueuesSkipUsageCheck() {
+      return autoDeleteQueuesSkipUsageCheck != null ? autoDeleteQueuesSkipUsageCheck : AddressSettings.DEFAULT_AUTO_DELETE_QUEUES_SKIP_USAGE_CHECK;
+   }
+
+   public AddressSettings setAutoDeleteQueuesSkipUsageCheck(final boolean autoDeleteQueuesSkipUsageCheck) {
+      this.autoDeleteQueuesSkipUsageCheck = autoDeleteQueuesSkipUsageCheck;
+      return this;
+   }
+
    public long getAutoDeleteQueuesMessageCount() {
       return autoDeleteQueuesMessageCount != null ? autoDeleteQueuesMessageCount : AddressSettings.DEFAULT_AUTO_DELETE_QUEUES_MESSAGE_COUNT;
    }
@@ -472,6 +491,15 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
 
    public AddressSettings setAutoDeleteAddressesDelay(final long autoDeleteAddressesDelay) {
       this.autoDeleteAddressesDelay = autoDeleteAddressesDelay;
+      return this;
+   }
+
+   public boolean isAutoDeleteAddressesSkipUsageCheck() {
+      return autoDeleteAddressesSkipUsageCheck != null ? autoDeleteAddressesSkipUsageCheck : AddressSettings.DEFAULT_AUTO_DELETE_ADDRESSES_SKIP_USAGE_CHECK;
+   }
+
+   public AddressSettings setAutoDeleteAddressesSkipUsageCheck(final boolean autoDeleteAddressesSkipUsageCheck) {
+      this.autoDeleteAddressesSkipUsageCheck = autoDeleteAddressesSkipUsageCheck;
       return this;
    }
 
@@ -1084,6 +1112,9 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
       if (autoDeleteQueuesDelay == null) {
          autoDeleteQueuesDelay = merged.autoDeleteQueuesDelay;
       }
+      if (autoDeleteQueuesSkipUsageCheck == null) {
+         autoDeleteQueuesSkipUsageCheck = merged.autoDeleteQueuesSkipUsageCheck;
+      }
       if (autoDeleteQueuesMessageCount == null) {
          autoDeleteQueuesMessageCount = merged.autoDeleteQueuesMessageCount;
       }
@@ -1098,6 +1129,9 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
       }
       if (autoDeleteAddressesDelay == null) {
          autoDeleteAddressesDelay = merged.autoDeleteAddressesDelay;
+      }
+      if (autoDeleteAddressesSkipUsageCheck == null) {
+         autoDeleteAddressesSkipUsageCheck = merged.autoDeleteAddressesSkipUsageCheck;
       }
       if (configDeleteAddresses == null) {
          configDeleteAddresses = merged.configDeleteAddresses;
@@ -1433,6 +1467,10 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
       if (buffer.readableBytes() > 0) {
          maxSizeMessages = BufferHelper.readNullableLong(buffer);
       }
+
+      if (buffer.readableBytes() > 0) {
+         autoDeleteAddressesSkipUsageCheck = BufferHelper.readNullableBoolean(buffer);
+      }
    }
 
    @Override
@@ -1482,7 +1520,9 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
          SimpleString.sizeofNullableString(defaultLastValueKey) +
          BufferHelper.sizeOfNullableBoolean(defaultNonDestructive) +
          BufferHelper.sizeOfNullableLong(autoDeleteQueuesDelay) +
+         BufferHelper.sizeOfNullableBoolean(autoDeleteQueuesSkipUsageCheck) +
          BufferHelper.sizeOfNullableLong(autoDeleteAddressesDelay) +
+         BufferHelper.sizeOfNullableBoolean(autoDeleteAddressesSkipUsageCheck) +
          BufferHelper.sizeOfNullableBoolean(defaultGroupRebalance) +
          BufferHelper.sizeOfNullableInteger(defaultGroupBuckets) +
          SimpleString.sizeofNullableString(defaultGroupFirstKey) +
@@ -1637,6 +1677,8 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
       buffer.writeNullableSimpleString(configDeleteDiverts != null ? new SimpleString(configDeleteDiverts.toString()) : null);
 
       BufferHelper.writeNullableLong(buffer, maxSizeMessages);
+
+      BufferHelper.writeNullableBoolean(buffer, autoDeleteAddressesSkipUsageCheck);
    }
 
    /* (non-Javadoc)
@@ -1679,11 +1721,13 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
       result = prime * result + ((autoDeleteQueues == null) ? 0 : autoDeleteQueues.hashCode());
       result = prime * result + ((autoDeleteCreatedQueues == null) ? 0 : autoDeleteCreatedQueues.hashCode());
       result = prime * result + ((autoDeleteQueuesDelay == null) ? 0 : autoDeleteQueuesDelay.hashCode());
+      result = prime * result + ((autoDeleteQueuesSkipUsageCheck == null) ? 0 : autoDeleteQueuesSkipUsageCheck.hashCode());
       result = prime * result + ((autoDeleteQueuesMessageCount == null) ? 0 : autoDeleteQueuesMessageCount.hashCode());
       result = prime * result + ((configDeleteQueues == null) ? 0 : configDeleteQueues.hashCode());
       result = prime * result + ((autoCreateAddresses == null) ? 0 : autoCreateAddresses.hashCode());
       result = prime * result + ((autoDeleteAddresses == null) ? 0 : autoDeleteAddresses.hashCode());
       result = prime * result + ((autoDeleteAddressesDelay == null) ? 0 : autoDeleteAddressesDelay.hashCode());
+      result = prime * result + ((autoDeleteAddressesSkipUsageCheck == null) ? 0 : autoDeleteAddressesSkipUsageCheck.hashCode());
       result = prime * result + ((configDeleteAddresses == null) ? 0 : configDeleteAddresses.hashCode());
       result = prime * result + ((configDeleteDiverts == null) ? 0 : configDeleteDiverts.hashCode());
       result = prime * result + ((managementBrowsePageSize == null) ? 0 : managementBrowsePageSize.hashCode());
@@ -1893,6 +1937,11 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
             return false;
       } else if (!autoDeleteQueuesDelay.equals(other.autoDeleteQueuesDelay))
          return false;
+      if (autoDeleteQueuesSkipUsageCheck == null) {
+         if (other.autoDeleteQueuesSkipUsageCheck != null)
+            return false;
+      } else if (!autoDeleteQueuesSkipUsageCheck.equals(other.autoDeleteQueuesSkipUsageCheck))
+         return false;
       if (autoDeleteQueuesMessageCount == null) {
          if (other.autoDeleteQueuesMessageCount != null)
             return false;
@@ -1917,6 +1966,11 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
          if (other.autoDeleteAddressesDelay != null)
             return false;
       } else if (!autoDeleteAddressesDelay.equals(other.autoDeleteAddressesDelay))
+         return false;
+      if (autoDeleteAddressesSkipUsageCheck == null) {
+         if (other.autoDeleteAddressesSkipUsageCheck != null)
+            return false;
+      } else if (!autoDeleteAddressesSkipUsageCheck.equals(other.autoDeleteAddressesSkipUsageCheck))
          return false;
       if (configDeleteAddresses == null) {
          if (other.configDeleteAddresses != null)
@@ -2162,6 +2216,8 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
          autoDeleteCreatedQueues +
          ", autoDeleteQueuesDelay=" +
          autoDeleteQueuesDelay +
+         ", autoDeleteQueuesSkipUsageCheck=" +
+         autoDeleteQueuesSkipUsageCheck +
          ", autoDeleteQueuesMessageCount=" +
          autoDeleteQueuesMessageCount +
          ", configDeleteQueues=" +
@@ -2172,6 +2228,8 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
          autoDeleteAddresses +
          ", autoDeleteAddressesDelay=" +
          autoDeleteAddressesDelay +
+         ", autoDeleteAddressesSkipUsageCheck=" +
+         autoDeleteAddressesSkipUsageCheck +
          ", configDeleteAddresses=" +
          configDeleteAddresses  +
          ", configDeleteDiverts=" +

artemis-commons/src/test/java/org/apache/activemq/artemis/utils/Wait.java
@@ -91,7 +91,7 @@ public class Wait {
 
 
    public static void assertEquals(Object obj, ObjectCondition condition, long timeout, long sleepMillis) throws Exception {
-      boolean result = waitFor(() -> (obj == condition || obj.equals(condition.getObject())), timeout, sleepMillis);
+      boolean result = waitFor(() -> (obj == condition || (obj != null && obj.equals(condition.getObject()))), timeout, sleepMillis);
 
       if (!result) {
          Assert.assertEquals(obj, condition.getObject());

artemis-server/src/main/java/org/apache/activemq/artemis/core/deployers/impl/FileConfigurationParser.java
@@ -276,6 +276,8 @@ public final class FileConfigurationParser extends XMLConfigurationUtil {
 
    private static final String AUTO_DELETE_QUEUES_MESSAGE_COUNT = "auto-delete-queues-message-count";
 
+   private static final String AUTO_DELETE_QUEUES_SKIP_USAGE_CHECK = "auto-delete-queues-skip-usage-check";
+
    private static final String CONFIG_DELETE_QUEUES = "config-delete-queues";
 
    private static final String AUTO_CREATE_ADDRESSES = "auto-create-addresses";
@@ -283,6 +285,8 @@ public final class FileConfigurationParser extends XMLConfigurationUtil {
    private static final String AUTO_DELETE_ADDRESSES = "auto-delete-addresses";
 
    private static final String AUTO_DELETE_ADDRESSES_DELAY = "auto-delete-addresses-delay";
+
+   private static final String AUTO_DELETE_ADDRESSES_SKIP_USAGE_CHECK = "auto-delete-addresses-skip-usage-check";
 
    private static final String CONFIG_DELETE_ADDRESSES = "config-delete-addresses";
 
@@ -325,8 +329,6 @@ public final class FileConfigurationParser extends XMLConfigurationUtil {
    private static final String ENABLE_METRICS = "enable-metrics";
 
    private static final String ENABLE_INGRESS_TIMESTAMP = "enable-ingress-timestamp";
-
-   private static final String SUPPRESS_SESSION_NOTIFICATIONS = "suppress-session-notifications";
 
    private boolean validateAIO = false;
 
@@ -1334,6 +1336,8 @@ public final class FileConfigurationParser extends XMLConfigurationUtil {
             long autoDeleteQueuesMessageCount = XMLUtil.parseLong(child);
             Validators.MINUS_ONE_OR_GE_ZERO.validate(AUTO_DELETE_QUEUES_MESSAGE_COUNT, autoDeleteQueuesMessageCount);
             addressSettings.setAutoDeleteQueuesMessageCount(autoDeleteQueuesMessageCount);
+         }  else if (AUTO_DELETE_QUEUES_SKIP_USAGE_CHECK.equalsIgnoreCase(name)) {
+            addressSettings.setAutoDeleteQueuesSkipUsageCheck(XMLUtil.parseBoolean(child));
          } else if (CONFIG_DELETE_QUEUES.equalsIgnoreCase(name)) {
             String value = getTrimmedTextContent(child);
             Validators.DELETION_POLICY_TYPE.validate(CONFIG_DELETE_QUEUES, value);
@@ -1347,6 +1351,8 @@ public final class FileConfigurationParser extends XMLConfigurationUtil {
             long autoDeleteAddressesDelay = XMLUtil.parseLong(child);
             Validators.GE_ZERO.validate(AUTO_DELETE_ADDRESSES_DELAY, autoDeleteAddressesDelay);
             addressSettings.setAutoDeleteAddressesDelay(autoDeleteAddressesDelay);
+         } else if (AUTO_DELETE_ADDRESSES_SKIP_USAGE_CHECK.equalsIgnoreCase(name)) {
+            addressSettings.setAutoDeleteAddressesSkipUsageCheck(XMLUtil.parseBoolean(child));
          } else if (CONFIG_DELETE_ADDRESSES.equalsIgnoreCase(name)) {
             String value = getTrimmedTextContent(child);
             Validators.DELETION_POLICY_TYPE.validate(CONFIG_DELETE_ADDRESSES, value);

artemis-server/src/main/java/org/apache/activemq/artemis/core/postoffice/AddressManager.java
@@ -83,13 +83,8 @@ public interface AddressManager {
 
    void scanAddresses(MirrorController mirrorController) throws Exception;
 
-   boolean checkAutoRemoveAddress(SimpleString address,
-                                  AddressInfo addressInfo,
+   boolean checkAutoRemoveAddress(AddressInfo addressInfo,
                                   AddressSettings settings,
                                   boolean ignoreDelay) throws Exception;
-
-   boolean checkAutoRemoveAddress(SimpleString address,
-                                  AddressInfo addressInfo,
-                                  AddressSettings settings) throws Exception;
 
 }

artemis-server/src/main/java/org/apache/activemq/artemis/core/postoffice/impl/PostOfficeImpl.java
@@ -1026,8 +1026,8 @@ public class PostOfficeImpl implements PostOffice, NotificationListener, Binding
 
    @Override
    public boolean isAddressBound(final SimpleString address) throws Exception {
-      Bindings bindings = lookupBindingsForAddress(address);
-      return bindings != null && !bindings.getBindings().isEmpty();
+      Collection<Binding> bindings = getDirectBindings(address);
+      return bindings != null && !bindings.isEmpty();
    }
 
    @Override
@@ -1902,15 +1902,16 @@ public class PostOfficeImpl implements PostOffice, NotificationListener, Binding
       }
    }
 
-   private static boolean queueWasUsed(Queue queue) {
-      return queue.getMessagesExpired() > 0 || queue.getMessagesAcknowledged() > 0 || queue.getMessagesKilled() > 0 || queue.getConsumerRemovedTimestamp() != -1;
+   private static boolean queueWasUsed(Queue queue, AddressSettings settings) {
+      return queue.getMessagesExpired() > 0 || queue.getMessagesAcknowledged() > 0 || queue.getMessagesKilled() > 0 || queue.getConsumerRemovedTimestamp() != -1 || settings.getAutoDeleteQueuesSkipUsageCheck();
    }
 
    /** To be used by the AddressQueueReaper.
     * It is also exposed for tests through PostOfficeTestAccessor */
    void reapAddresses(boolean initialCheck) {
       getLocalQueues().forEach(queue -> {
-         if (!queue.isInternalQueue() && QueueManagerImpl.isAutoDelete(queue) && QueueManagerImpl.consumerCountCheck(queue) && (initialCheck || QueueManagerImpl.delayCheck(queue)) && QueueManagerImpl.messageCountCheck(queue) && (initialCheck || queueWasUsed(queue))) {
+         AddressSettings settings = addressSettingsRepository.getMatch(queue.getAddress().toString());
+         if (!queue.isInternalQueue() && queue.isAutoDelete() && QueueManagerImpl.consumerCountCheck(queue) && (initialCheck || QueueManagerImpl.delayCheck(queue, settings)) && QueueManagerImpl.messageCountCheck(queue) && (initialCheck || queueWasUsed(queue, settings))) {
             if (initialCheck || queue.isSwept()) {
                if (logger.isDebugEnabled()) {
                   if (initialCheck) {
@@ -1935,7 +1936,7 @@ public class PostOfficeImpl implements PostOffice, NotificationListener, Binding
          AddressSettings settings = addressSettingsRepository.getMatch(address.toString());
 
          try {
-            if (addressManager.checkAutoRemoveAddress(address, addressInfo, settings, initialCheck)) {
+            if (addressManager.checkAutoRemoveAddress(addressInfo, settings, initialCheck)) {
                if (initialCheck || addressInfo.isSwept()) {
 
                   server.autoRemoveAddressInfo(address, null);
@@ -1965,12 +1966,6 @@ public class PostOfficeImpl implements PostOffice, NotificationListener, Binding
             }
          }
       }
-   }
-
-   public boolean checkAutoRemoveAddress(SimpleString address,
-                              AddressInfo addressInfo,
-                              AddressSettings settings) throws Exception {
-      return settings.isAutoDeleteAddresses() && addressInfo != null && addressInfo.isAutoCreated() && !isAddressBound(address) && addressInfo.getBindingRemovedTimestamp() != -1 && (System.currentTimeMillis() - addressInfo.getBindingRemovedTimestamp() >= settings.getAutoDeleteAddressesDelay());
    }
 
    private Stream<Queue> getLocalQueues() {

artemis-server/src/main/java/org/apache/activemq/artemis/core/postoffice/impl/SimpleAddressManager.java
@@ -366,17 +366,18 @@ public class SimpleAddressManager implements AddressManager {
    }
 
    @Override
-   public boolean checkAutoRemoveAddress(SimpleString address,
-                                         AddressInfo addressInfo,
-                                         AddressSettings settings) throws Exception {
-      return checkAutoRemoveAddress(address, addressInfo, settings, false);
+   public boolean checkAutoRemoveAddress(AddressInfo addressInfo,
+                                         AddressSettings settings,
+                                         boolean ignoreDelay) throws Exception {
+      return settings.isAutoDeleteAddresses() && addressInfo != null && addressInfo.isAutoCreated() && !bindingsFactory.isAddressBound(addressInfo.getName()) && addressWasUsed(addressInfo, settings) && (ignoreDelay || delayCheck(addressInfo, settings));
    }
 
-   @Override
-   public boolean checkAutoRemoveAddress(SimpleString address,
-                                         AddressInfo addressInfo,
-                                         AddressSettings settings, boolean ignoreDelay) throws Exception {
-      return settings.isAutoDeleteAddresses() && addressInfo != null && addressInfo.isAutoCreated() && !bindingsFactory.isAddressBound(address) && (ignoreDelay || addressInfo.getBindingRemovedTimestamp() != -1 && (System.currentTimeMillis() - addressInfo.getBindingRemovedTimestamp() >= settings.getAutoDeleteAddressesDelay()));
+   private boolean delayCheck(AddressInfo addressInfo, AddressSettings settings) {
+      return (!settings.isAutoDeleteAddressesSkipUsageCheck() && System.currentTimeMillis() - addressInfo.getBindingRemovedTimestamp() >= settings.getAutoDeleteAddressesDelay()) || (settings.isAutoDeleteAddressesSkipUsageCheck() && System.currentTimeMillis() - addressInfo.getCreatedTimestamp() >= settings.getAutoDeleteAddressesDelay());
+   }
+
+   private boolean addressWasUsed(AddressInfo addressInfo, AddressSettings settings) {
+      return addressInfo.getBindingRemovedTimestamp() != -1 || settings.isAutoDeleteAddressesSkipUsageCheck();
    }
 
    @Override

artemis-server/src/main/java/org/apache/activemq/artemis/core/server/ActiveMQServerLogger.java
@@ -2201,11 +2201,11 @@ public interface ActiveMQServerLogger extends BasicLogger {
    void useOnlyAllowList();
 
    @LogMessage(level = Logger.Level.INFO)
-   @Message(id = 224112, value = "Auto removing Queue {0} with queueID={1} on address={2}", format = Message.Format.MESSAGE_FORMAT)
+   @Message(id = 224112, value = "Auto removing queue {0} with queueID={1} on address={2}", format = Message.Format.MESSAGE_FORMAT)
    void autoRemoveQueue(String name, long queueID, String address);
 
    @LogMessage(level = Logger.Level.INFO)
-   @Message(id = 224113, value = "Auto removing Address {0}", format = Message.Format.MESSAGE_FORMAT)
+   @Message(id = 224113, value = "Auto removing address {0}", format = Message.Format.MESSAGE_FORMAT)
    void autoRemoveAddress(String name);
 
    @LogMessage(level = Logger.Level.INFO)

artemis-server/src/main/java/org/apache/activemq/artemis/core/server/Queue.java
@@ -532,4 +532,8 @@ public interface Queue extends Bindable,CriticalComponent {
    default QueueConfiguration getQueueConfiguration() {
       return null;
    }
+
+   default long getCreatedTimestamp() {
+      return -1;
+   }
 }

commit 6c885c2531f6530d917975fa6f81cedeb1b32d88

ARTEMIS-4162 Fix test failures caused by d61126bb380c3de72422a3b60d2ed6033e1ca107
downstream: ENTMQBR-7866

artemis-server/src/main/java/org/apache/activemq/artemis/core/settings/impl/AddressSettings.java
@@ -1469,6 +1469,10 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
       }
 
       if (buffer.readableBytes() > 0) {
+         autoDeleteQueuesSkipUsageCheck = BufferHelper.readNullableBoolean(buffer);
+      }
+
+      if (buffer.readableBytes() > 0) {
          autoDeleteAddressesSkipUsageCheck = BufferHelper.readNullableBoolean(buffer);
       }
    }
@@ -1677,6 +1681,8 @@ public class AddressSettings implements Mergeable<AddressSettings>, Serializable
       buffer.writeNullableSimpleString(configDeleteDiverts != null ? new SimpleString(configDeleteDiverts.toString()) : null);
 
       BufferHelper.writeNullableLong(buffer, maxSizeMessages);
+
+      BufferHelper.writeNullableBoolean(buffer, autoDeleteQueuesSkipUsageCheck);
 
       BufferHelper.writeNullableBoolean(buffer, autoDeleteAddressesSkipUsageCheck);
    }