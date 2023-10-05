99e6bf7e6a177d0e9ff4168bfc4b05f31cb3ebfe
Clebert Suconic <clebertsuconic@apache.org>
2023-10-05 11:43:35 -0400

    ARTEMIS-4450 Caching hasLocal boolean on bindings to avoid transversing the maps on every call
    (cherry picked from commit 42be518deb538ad67c8b12afb70ea12144b6f4a0)
    
    downstream: ENTMQBR-8471

--- tests/unit-tests/src/test/java/org/apache/activemq/artemis/tests/unit/core/postoffice/impl/WildcardAddressManagerUnitTest.java
+++ tests/unit-tests/src/test/java/org/apache/activemq/artemis/tests/unit/core/postoffice/impl/WildcardAddressManagerUnitTest.java
@@ -441,6 +441,11 @@ public class WildcardAddressManagerUnitTest extends ActiveMQTestBase {
       }
 
       @Override
+      public boolean hasLocalBinding() {
+         return false;
+      }
+
+      @Override
       public Collection<Binding> getBindings() {
          return bindings.values();
       }
