a4e30395cb0d13e96d9bc954c9e21f3335a4e033
Domenico Francesco Bruscino <brusdev@apache.org>
2024-02-02 08:05:32 +0100

    ARTEMIS-4436 Remove unused import java.lang.invoke.MethodHandles
    downstream: ENTMQBR-8494

--- tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/cluster/warnings/ClusterCleanNodeShutdownTest.java
+++ tests/integration-tests/src/test/java/org/apache/activemq/artemis/tests/integration/cluster/warnings/ClusterCleanNodeShutdownTest.java
@@ -16,7 +16,6 @@
  */
 package org.apache.activemq.artemis.tests.integration.cluster.warnings;
 
-import java.lang.invoke.MethodHandles;
 import org.apache.activemq.artemis.core.server.cluster.impl.MessageLoadBalancingType;
 import org.apache.activemq.artemis.logs.AssertionLoggerHandler;
 import org.apache.activemq.artemis.tests.integration.cluster.distribution.ClusterTestBase;
