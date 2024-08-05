c7a6c57ead9c35930b1fafeb910f9118e00c2e6f
Howard Gao <howard.gao@gmail.com>
2024-08-05 20:44:29 +0800

    ARTEMIS-4973 pageSizeBytes/pageLimitBytes combination can cause Address full Update docs/user-manual/paging.adoc
    Co-authored-by: Robbie Gemmell <robbie@apache.org>
    (cherry picked from commit 1f79341c05d9e512cfd41e828c00bd01ca470fbf)
    
    downstream: ENTMQBR-9233

--- artemis-server/src/main/java/org/apache/activemq/artemis/core/paging/cursor/impl/PageCursorProviderImpl.java
+++ artemis-server/src/main/java/org/apache/activemq/artemis/core/paging/cursor/impl/PageCursorProviderImpl.java
@@ -250,7 +250,7 @@ public class PageCursorProviderImpl implements PageCursorProvider {
       scheduleCleanup();
    }
 
-   private long getNumberOfMessagesOnSubscriptions() {
+   protected long getNumberOfMessagesOnSubscriptions() {
       AtomicLong largerCounter = new AtomicLong();
       activeCursors.forEach((id, sub) -> {
          long value = sub.getCounter().getValue();
@@ -262,7 +262,8 @@ public class PageCursorProviderImpl implements PageCursorProvider {
       return largerCounter.get();
    }
 
-   void checkClearPageLimit() {
+   @Override
+   public void checkClearPageLimit() {
       pagingStore.checkPageLimit(getNumberOfMessagesOnSubscriptions());
    }
 
