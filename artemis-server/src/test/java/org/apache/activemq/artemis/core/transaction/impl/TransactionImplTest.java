a568d64659299765086aeb9cb8fc6fd5ba9f2d85
Clebert Suconic <clebertsuconic@apache.org>
2024-08-22 21:39:08 -0400

    ARTEMIS-5010 AckManager records are not replicated
    (cherry picked from commit 4ba9f67d80101878b75f27d0247a8e46303e931a)
    
    downstream: ENTMQBR-9394
    (cherry picked from commit c95188377a8ec0422b8c2ce429f85a887855b6fe)

--- artemis-server/src/test/java/org/apache/activemq/artemis/core/transaction/impl/TransactionImplTest.java
+++ artemis-server/src/test/java/org/apache/activemq/artemis/core/transaction/impl/TransactionImplTest.java
@@ -31,6 +31,7 @@ import org.apache.activemq.artemis.api.core.Pair;
 import org.apache.activemq.artemis.api.core.SimpleString;
 import org.apache.activemq.artemis.core.io.IOCallback;
 import org.apache.activemq.artemis.core.io.SequentialFile;
+import org.apache.activemq.artemis.core.journal.IOCompletion;
 import org.apache.activemq.artemis.core.journal.Journal;
 import org.apache.activemq.artemis.core.journal.JournalLoadInformation;
 import org.apache.activemq.artemis.core.journal.RecordInfo;
@@ -43,6 +44,7 @@ import org.apache.activemq.artemis.core.persistence.AddressBindingInfo;
 import org.apache.activemq.artemis.core.persistence.AddressQueueStatus;
 import org.apache.activemq.artemis.core.persistence.GroupingInfo;
 import org.apache.activemq.artemis.core.persistence.OperationContext;
+import org.apache.activemq.artemis.core.persistence.Persister;
 import org.apache.activemq.artemis.core.persistence.QueueBindingInfo;
 import org.apache.activemq.artemis.core.persistence.StorageManager;
 import org.apache.activemq.artemis.core.persistence.config.AbstractPersistedAddressSetting;
@@ -938,7 +940,31 @@ public class TransactionImplTest extends ServerTestBase {
 
       @Override
       public void deleteAddressStatus(long recordID) throws Exception {
+      }
 
+      @Override
+      public void storeMapRecord(long id,
+                                 byte recordType,
+                                 Persister persister,
+                                 Object record,
+                                 boolean sync,
+                                 IOCompletion completionCallback) throws Exception {
+      }
+
+      @Override
+      public void storeMapRecord(long id,
+                                 byte recordType,
+                                 Persister persister,
+                                 Object record,
+                                 boolean sync) throws Exception {
+      }
+
+      @Override
+      public void deleteMapRecord(long id, boolean sync) throws Exception {
+      }
+
+      @Override
+      public void deleteMapRecordTx(long txid, long id) throws Exception {
       }
    }
 
