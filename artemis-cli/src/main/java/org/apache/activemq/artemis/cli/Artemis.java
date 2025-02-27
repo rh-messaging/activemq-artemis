ce1e68fbc83b22df74638265cf1e33b3a7fc85e6
rh-messaging-ci <messaging-infra@redhat.com>
2025-02-27 16:57:46 +0000

    RH-PATCH Apply: disable the shell

--- artemis-cli/src/main/java/org/apache/activemq/artemis/cli/Artemis.java
+++ artemis-cli/src/main/java/org/apache/activemq/artemis/cli/Artemis.java
@@ -87,9 +87,10 @@ public class Artemis implements Runnable {
 
    @Override
    public void run() {
-      // We are running the shell by default.
-      // if you type ./artemis we will go straight to the shell
-      Shell.runShell(true);
+      // We are running the help by default.
+      boolean isInstance = System.getProperty("artemis.instance") != null;
+      CommandLine commandLine = buildCommand(isInstance, !isInstance, false);
+      HelpAction.help(commandLine, null);
    }
 
    public static void main(String... args) throws Exception {
@@ -263,11 +264,6 @@ public class Artemis implements Runnable {
 
       commandLine.addSubcommand(new AutoCompletion());
 
-      // we don't include the shell in the shell
-      if (shellEnabled) {
-         commandLine.addSubcommand(new Shell(commandLine));
-      }
-
       commandLine.addSubcommand(new Producer()).addSubcommand(new Transfer()).addSubcommand(new Consumer()).addSubcommand(new Browse()).addSubcommand(new Mask()).addSubcommand(new PrintVersion());
 
       commandLine.addSubcommand(new PerfGroup(commandLine));
@@ -275,19 +271,12 @@ public class Artemis implements Runnable {
       commandLine.addSubcommand(new QueueGroup(commandLine));
       commandLine.addSubcommand(new AddressGroup(commandLine));
 
-      if (Shell.inShell()) {
-         commandLine.addSubcommand(new Connect());
-         commandLine.addSubcommand(new Disconnect());
-      }
-
       if (includeInstanceCommands) {
          commandLine.addSubcommand(new ActivationGroup(commandLine));
          commandLine.addSubcommand(new DataGroup(commandLine));
          commandLine.addSubcommand(new UserGroup(commandLine));
 
-         if (!Shell.inShell()) {
-            commandLine.addSubcommand(new Run());
-         }
+         commandLine.addSubcommand(new Run());
 
          commandLine.addSubcommand(new Stop());
          commandLine.addSubcommand(new Kill());
